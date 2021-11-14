acc084a673d7458e89804511a849476e5bc28046
erin <erinverkoulen93@gmail.com>
2021-11-14 17:01:15 -0600

    commit tree 6723786babc66d1df2c9ad44161f893ff0e242cf

--- app/models/dog_house.rb
+++ app/models/dog_house.rb
@@ -1,3 +1,5 @@
 class DogHouse < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
+
+#dependent: :destroy is an additional option that tells ActiveRecord to delete all the associated records when the parent record is deleted. 
